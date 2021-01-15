1431534234708ed207852ad5e808efdcdd242aba
Wagner <wagner@hangar.digital>
2021-01-15 18:53:43 -0300

    [COMMIT INIT]

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Wagner"
+    },
+    {
+        id: 2,
+        name: "Mateus"
+    },
+    {
+        id: 3,
+        name: "João"
+    },
+];
